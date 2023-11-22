c491c38854a4ef8b0d2babd080b93a08c2877585
Kristjan Lääne <xonproject@gmail.com>
2023-11-22 22:45:01 +0200

    cartscreen component start and route

---
+++ frontend/src/screens/CartScreen.jsx
@@ -0,0 +1,23 @@
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from 'react-bootstrap';
+import { FaTrash } from 'react-icons/fa';
+import Message from '../components/Message';
+
+export const CartScreen = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  return <div>CartScreen</div>;
+};
